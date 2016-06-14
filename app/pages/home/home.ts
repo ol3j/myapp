5bba37116f09f08dd40b34a3cf6eb069444b688d
Łukasz Olejarczuk <ol3j@o2.pl>
2016-06-15 00:24:41 +0200

    added momentjs & typings

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,4 +1,4 @@
-
+import * as moment from 'moment';
 import {Component} from "@angular/core";
 import {NavController} from 'ionic-angular';
 
@@ -7,7 +7,8 @@ import {NavController} from 'ionic-angular';
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-
+  date:string;
   constructor(private _navController: NavController) {
+    this.date = moment().add("days", 7).format("dd/MM/YYYY");
   }
 }
