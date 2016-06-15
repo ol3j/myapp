d00c74edde48bb2d3bdcb75ce79906fccf238f87
Łukasz Olejarczuk <ol3j@o2.pl>
2016-06-15 21:58:30 +0200

    introduced alert [add alert]

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,6 +1,7 @@
 import * as moment from 'moment';
 import {Component} from "@angular/core";
-import {NavController} from 'ionic-angular';
+//Add alert
+import {Alert, NavController} from 'ionic-angular';
 
 
 @Component({
@@ -8,7 +9,40 @@ import {NavController} from 'ionic-angular';
 })
 export class HomePage {
   date:string;
+  winner: string;
+
   constructor(private _navController: NavController) {
     this.date = moment().add("days", 7).format("dd/MM/YYYY");
+  }
+
+  //add a action
+  doPrompt() {
+    let prompt = Alert.create({
+      title: 'The winner Euro 2016',
+      message: "Enter the winner",
+      inputs: [
+        {
+          name: 'winner',
+          placeholder: 'The Winner is...'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.winner = data.winner;
+          }
+        }
+      ]
+    });
+
+    //The present method is used by overlays, present will place the overlay in the root NavController's stack. 
+    this._navController.present(prompt);
   }
 }
