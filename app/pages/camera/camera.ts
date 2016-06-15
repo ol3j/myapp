57f6438cbb2c1502890da2f1f41f11c8d7edbdd7
Łukasz Olejarczuk <ol3j@o2.pl>
2016-06-16 00:31:13 +0200

    take picture [Take picture]

--- app/pages/camera/camera.ts
+++ app/pages/camera/camera.ts
@@ -1,9 +1,32 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {Alert, NavController} from 'ionic-angular';
+//add Camera from ionic native
+import {Camera} from 'ionic-native';
 
 @Component({
   templateUrl: 'build/pages/camera/camera.html',
 })
 export class CameraPage {
-  constructor(public nav: NavController) {}
+  public base64Image: string;
+
+  constructor(public nav: NavController) { }
+
+  //add action take a picture
+  takePicture() {
+    Camera.getPicture({
+      destinationType: Camera.DestinationType.DATA_URL
+    })
+      .then((image) => {
+        //image is a base64 encoded string
+        this.base64Image = "data:image/jpeg;base64," + image;
+      }, (error) => {
+        //display alert if error
+        let alert = Alert.create({
+          title: 'Ups. Error occurs!',
+          subTitle: 'Sorry, we have a error. Please try later or fix the bug.',
+          buttons: ['OK']
+        });
+        this.nav.present(alert);
+      });
+  }
 }
