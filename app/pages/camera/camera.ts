a802ae9498730c1f4c61385a54f8233802e43a56
Łukasz Olejarczuk <ol3j@o2.pl>
2016-06-15 22:45:34 +0200

    generate camera page [CameraPage]

---
+++ app/pages/camera/camera.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+
+@Component({
+  templateUrl: 'build/pages/camera/camera.html',
+})
+export class CameraPage {
+  constructor(public nav: NavController) {}
+}
