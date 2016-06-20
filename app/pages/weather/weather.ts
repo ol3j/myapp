f9eedfeb9acd2d5c27ff8bb33f27a334b24b0842
Łukasz Olejarczuk <ol3j@o2.pl>
2016-06-20 23:56:43 +0200

    introduced weather page and provider

---
+++ app/pages/weather/weather.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+
+/*
+  Generated class for the WeatherPage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  templateUrl: 'build/pages/weather/weather.html',
+})
+export class WeatherPage {
+  constructor(public nav: NavController) {}
+}
